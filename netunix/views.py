6fa457bb75fdc2f507010ac3f318955fd0cd3648
Mike <mikecummings221@gmail.com>
2018-05-16 00:26:45 -0600

    torealdate

--- netunix/views.py
+++ netunix/views.py
@@ -1,36 +1,52 @@
 
 from datetime import *
+import time
 
-class dotnet_unix(object):
-	def __init__(self, unix=None, dotnet=None, real_date=None):
+class dotnet_unix(object):		
+	def __init__(self, unix=None, real_date=None, dotnet=None):
 		self.unix = unix
+		self.real_date = real_date
 		self.dotnet = dotnet
-		self.real_date=real_date
+
+
 	def now(self):
-		if (self.unix is None and self.dotnet is None):
+		if self.unix is None and self.real_date is None and self.dotnet is None:
 			dotnet = datetime(1,1,1)
-			g = datetime.now()
-			unix = g - dotnet
+			g = datetime.now
+			unix = g() - dotnet
 			dotnettimestamp = (((unix.days*60*60*24 + unix.seconds)*1000 + unix.microseconds)*100)*100
 			return dotnettimestamp
-	def fromreal(self):
-		if (self.real_date is not None):
-			dotnet = datetime(1,1,1)
-			f = "%d/%m/%Y %H:%M:%S"
-			times = datetime.strptime(real_date, f)
-			unix = times - dotnet
-			dotnettimestamp = (((unix.days*60*60*24 + unix.seconds)*1000 + unix.microseconds)*100)*100
-			return dotnettimestamp
+
+	def from_realdate(self, real_date):
+		dotnet = datetime(1,1,1)
+		f = "%d/%m/%Y %H:%M:%S"
+		times = datetime.strptime(real_date, f)
+		unix = times - dotnet
+		dotnettimestamp = (((unix.days*60*60*24 + unix.seconds)*1000 + unix.microseconds)*100)*100
+		return dotnettimestamp
+
 	def from_unix(self, unix):
 		dotnet = datetime(1,1,1)
 		g = datetime.fromtimestamp(float(unix))
 		d = g - dotnet
 		dotnettimestamp = (((d.days*60*60*24 + d.seconds)*1000 + d.microseconds)*100)*100
 		return dotnettimestamp
-		
+
 	def from_dotnet(self, dotnet):
-		delta_epoch = datetime(1970,1,1)-datetime(1,1,1)
-		desec=timedelta.total_seconds(delta_epoch)
-		dotnettimestamp = (dotnet/10000000) - desec
-		return dotnettimestamp
+		if dotnet:
+			delta_epoch = (datetime(1970,1,1)) - (datetime(1,1,1))
+			desec=timedelta.total_seconds(delta_epoch)
+			unixtimestamp = (dotnet/10000000) - desec
+			return unixtimestamp
+
+
+	def to_realdate(self, torealdate):
+		unixconvert = self.from_dotnet(torealdate)
+		realdate  = datetime.fromtimestamp(unixconvert)
+		return realdate
+
+
+	
+
+
 # Create your views here.
